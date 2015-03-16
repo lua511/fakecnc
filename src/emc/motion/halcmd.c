1fb9ab810a2c08339a0b9fa354be953dd3802e26
root <lua511@163.com>
2015-03-17 03:15:23 +0800

    ok,linuxcnc.in ready,great.

--- src/emc/motion/halcmd.c
+++ src/emc/motion/halcmd.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 int main(int v,char** c){
-    printf("i am halcmd\n");
+    int i = 0;
+    time_t rawtime;
+    struct tm* timeinfo;
+    if(1) return 0;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    printf("halcmd:%d,",v);
+    for(i = 0; i < v; ++i){
+        if(i > 0) printf(" ");
+        printf("%s",c[i]);
+    }
+    printf(" %s ",asctime(timeinfo));
+    printf("\n");
     return 0;
 }
--- src/libnml/inifile/inivar.c
+++ src/libnml/inifile/inivar.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main(int v,char** c){
+    int i = 0;
+    if(v >= 6){
+        if(c[6][0] == 'D' && c[6][1] == 'I' && c[6][2] == 'S'){
+            printf("testnothing\n");
+            return 0;
+        }
+    }
     printf("fmilltask\n");
+    for(i = 1;i < v; ++i){
+        printf("%s ",c[i]);
+    }
+    printf("\n");
     return 0;
 }
