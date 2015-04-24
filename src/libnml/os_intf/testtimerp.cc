b451a5eb9c62781be0dca83e1104633bbb1891c8
root <lua511@163.com>
2015-04-24 20:10:03 +0800

    the doc says it will work,but,not

--- src/libnml/os_intf/testtimerp.cc
+++ src/libnml/os_intf/testtimerp.cc
@@ -21,5 +21,15 @@ int main(int v, char* c[])
 	printf("===end sleep===\n");
 	printf("diff: [%f]\n", dt2 - dt1);
 	printf("diff: [%f]\n", dt3 - dt2);
+	// the doc says this will work.but actually,it not
+	printf("***5 seconds***\n");
+	dt1 = etime();
+	RCS_TIMER* tm = new RCS_TIMER(5.0);
+	while(0){
+		tm->wait();
+	}
+	dt2 = etime();
+	delete tm;
+	printf("***[%f]***\n",dt2 - dt1);
 	return 0;
 }
